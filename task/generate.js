ef70e6538a057461edd78972f8cc71c56dbb5e5a
MasatoMakino <unagiinu.g@gmail.com>
2019-01-04 17:14:57 +0900

    フォルダからのxlsxドキュメント出力機能を追加。

---
+++ task/generate.js
@@ -0,0 +1,108 @@
+const path = require("path");
+const glob = require("glob");
+const xl = require("excel4node");
+const sizeof = require("image-size");
+const sharp = require("sharp");
+
+const imgDir = "./images";
+const distDir = "./dist";
+
+const list = glob.sync("**/*.+(jpg|jpeg|gif|png|JPG|JPEG|GIF|PNG)", {
+  cwd: imgDir
+});
+const SCALE_H = 0.75;
+const SCALE_W = 0.125;
+
+//xlsxファイルの初期化
+const workbook = new xl.Workbook();
+const sheet = workbook.addWorksheet("Sheet 1");
+
+//ヘッダー書き込み
+const initHeader = sheet => {
+  const headers = ["Path", "FileName", "image", "width", "height"];
+  headers.forEach((message, index) => {
+    sheet.cell(1, index + 1).string(message);
+  });
+};
+initHeader(sheet);
+
+/**
+ * 画像の縦横サイズを制限する。
+ * @param size
+ * @param size.width
+ * @param size.height
+ * @return {*} resize
+ * @return resize.width
+ * @return resize.height
+ */
+const limitSize = size => {
+  const maxH = 320;
+  if (size.height <= maxH) return size;
+
+  const resize = Object.assign({}, size);
+  const rate = maxH / resize.height;
+  resize.height = maxH;
+  resize.width = Math.floor(rate * resize.width);
+  return resize;
+};
+
+async function setImage(filePath, size, rowIndex) {
+  return new Promise((resolve, reject) => {
+    let resize = limitSize(size);
+    resizeCell(sheet, resize, rowIndex);
+
+    sharp(filePath)
+      .resize(resize.width, resize.height)
+      .toBuffer()
+      .then(data => {
+        sheet.addImage({
+          image: data,
+          type: "picture",
+          position: {
+            type: "oneCellAnchor",
+            from: {
+              col: 3,
+              // colOff: 1,
+              row: rowIndex
+              // rowOff: 1
+            }
+          }
+        });
+        resolve();
+      });
+  });
+}
+
+let maxW = 0;
+const resizeCell = (sheet, size, rowIndex) => {
+  sheet.row(rowIndex).setHeight(size.height * SCALE_H);
+  if (maxW < size.width) {
+    maxW = size.width;
+    sheet.column(3).setWidth(size.width * SCALE_W);
+  }
+};
+
+async function writeLine(imgPath, index) {
+  const filePath = path.resolve(imgDir, imgPath);
+  const size = sizeof(filePath);
+  const filename = path.basename(imgPath);
+
+  const rowIndex = index + 2;
+  sheet.cell(rowIndex, 1).string(imgPath);
+  sheet.cell(rowIndex, 2).string(filename);
+  sheet.cell(rowIndex, 4).number(size.width);
+  sheet.cell(rowIndex, 5).number(size.height);
+  await setImage(filePath, size, rowIndex);
+}
+
+//各行の書き込み
+const promises = [];
+list.forEach((imgPath, index) => {
+  promises.push(writeLine(imgPath, index));
+});
+
+Promise.all(promises).then(() => {
+  const xlsxPath = path.resolve(distDir, "images.xlsx");
+  workbook.write(xlsxPath);
+  console.log("write");
+});
