daee5df21d982792b0ec6f7b59e15033b537e35f
MasatoMakino <makino.masato.g@gmail.com>
2023-12-22 16:32:31 +0900

    change : share sharp object

--- bin/loadImageBuffer.js
+++ bin/loadImageBuffer.js
@@ -5,15 +5,15 @@ const sharp = require("sharp");
 
 /**
  * 指定されたパスの画像を指定サイズにリサイズする。
- * @param {string} path 画像ファイルのパス
- * @param size 画像のリサイズ後のサイズ
+ * @param {sharp.Sharp} sharpObj
+ * @param {Object} size 画像のリサイズ後のサイズ
  * @param {number} size.width
  * @param {number} size.height
  * @return {Promise<any>}
  */
-exports.loadImageBuffer = function (path, size) {
+exports.loadImageBuffer = function (sharpObj, size) {
   return new Promise((resolve, reject) => {
-    sharp(path)
+    sharpObj
       .resize(size.width, size.height)
       .toBuffer()
       .then((data) => {
@@ -24,15 +24,13 @@ exports.loadImageBuffer = function (path, size) {
 
 /**
  * 画像のメタデータを読み込む。
- * @param {string} path
+ * @param {sharp.Sharp} sharpObj
  * @returns {Promise<sharp.Metadata>}
  */
-exports.getImageMetadata = function (path) {
+exports.getImageMetadata = function (sharpObj) {
   return new Promise((resolve, reject) => {
-    sharp(path)
-      .metadata()
-      .then((metadata) => {
-        resolve(metadata);
-      });
+    sharpObj.metadata().then((metadata) => {
+      resolve(metadata);
+    });
   });
 };
