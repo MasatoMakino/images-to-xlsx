db85e0a17b3d2100f5e592a520934fd5e6ebf7c2
MasatoMakino <makino.masato.g@gmail.com>
2024-03-04 15:50:53 +0900

    bump : commander v11.1.0 to v12.0.0

--- bin/index.js
+++ bin/index.js
@@ -2,11 +2,16 @@
 "use strict";
 
 const generate = require("./generate.js");
-const program = require("commander");
+const { program } = require("commander");
 
 program
   .requiredOption("-i, --input <inputPath>", "image directory", String)
-  .option("-o, --output <outputPath>", "output file path", String, "./images.xlsx")
+  .option(
+    "-o, --output <outputPath>",
+    "output file path",
+    String,
+    "./images.xlsx",
+  )
   .parse();
 
 const options = program.opts();
